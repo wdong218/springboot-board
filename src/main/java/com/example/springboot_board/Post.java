e841fe993333ded21d2e5f1b2bebebea3a05e3a8
wdong218 <wdh8890@naver.com>
2025-06-27 21:16:55 +0900

    feat: Create Post domain entity

---
+++ src/main/java/com/example/springboot_board/Post.java
@@ -0,0 +1,25 @@
+package com.example.springboot_board;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity //이 클래스가 테이블임을 명시
+@EntityListeners(AuditingEntityListener.class)
+public class Post {
+
+    @Id //기본키로 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //자동으로 초기화 값 설정
+     private Long id;
+    @Column(nullable = false, length = 100) //제약조건 NUll금지,길이 제한
+     private String title;
+     private String content;
+     private String author;
+    @CreatedDate //insert 시 자동 생성
+     private LocalDateTime created_at;
+    @LastModifiedDate //업데이트 시 자동 갱신
+     private LocalDateTime  updated_at;
+}
